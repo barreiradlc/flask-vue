c6fe0050979ca957904579f4f95e59f4f17faf0f
Barreira <barreira266@hotmail.com>
2025-12-17 12:23:24 -0300

    WIP, backend connection 4 (Add cors)

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+
+CORS(app)
 
 # Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
